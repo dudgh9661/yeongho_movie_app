136c2b2e761ae72c3623f2584f4445e79c1776c8
Kimyeongho <dudgh9661@naver.com>
2020-07-11 00:20:23 +0900

    Updates

---
+++ precache-manifest.a406c320ecea0f3cabbf44451aab1941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d96173e1ae38862c4b5b2598324f17a",
+    "url": "/yeongho_movie_app/index.html"
+  },
+  {
+    "revision": "faf16c006d7c6e8140eb",
+    "url": "/yeongho_movie_app/static/css/main.f7ce751b.chunk.css"
+  },
+  {
+    "revision": "e9ff39a27fe7be9a07bc",
+    "url": "/yeongho_movie_app/static/js/2.bf9265b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yeongho_movie_app/static/js/2.bf9265b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf16c006d7c6e8140eb",
+    "url": "/yeongho_movie_app/static/js/main.d0986ca5.chunk.js"
+  },
+  {
+    "revision": "9e21310c737a9d724b34",
+    "url": "/yeongho_movie_app/static/js/runtime-main.20ecca6d.js"
+  }
+]);
